6ee6a622eb02d56db8cd8df8212b53abb88eb039
Roman Nuterman <nutrik@gmail.com>
2023-07-27 14:46:49 +0900

    fix doc again

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,10 +12,14 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    # TODO: remove this once fixed upstream
+    # see https://github.com/readthedocs/sphinx_rtd_theme/issues/1452
+    'sphinxcontrib.jquery',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.automodule',
     'sphinx.ext.intersphinx',
 ]
 
